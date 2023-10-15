948fc2a098877882d3dd1bbd6179dd1ad83d2dc0
suda <mohapatrasudam19@gmail.com>
2023-10-15 19:30:31 +0530

    jhwj

---
+++ assagnnt.java
@@ -0,0 +1,17 @@
+class Sum{
+    public static void main(String[] args)
+    {
+        int n=5678;
+        int sum=0;
+        while(n>0)
+        {
+            int rem=n%10;
+            n=n/10;
+            if(rem%2==0)
+            {
+                sum=sum+rem;
+            }
+            System.out.print(sum);
+        }
+    }
+}
